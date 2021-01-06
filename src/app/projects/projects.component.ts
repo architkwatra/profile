a799b657c14354f51234198140fbd2bbd4d1483c
archit kwatra <qrchit.kwatra@gmail.com>
2021-01-07 00:11:30 +0530

    update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,7 +9,28 @@ export class ProjectsComponent implements OnInit {
 
   constructor() { }
 
+  projectsURL = {
+    leafWilting: "https://github.com/architkwatra/Leaf-Wilting-Detection-in-Soybean-Plants/blob/master/TransferLearning.ipynb",
+    xinu: {
+      PA0: "https://github.com/architkwatra/CSC-501-Operating_Systems/tree/master/PA0", 
+      PA1: "https://github.com/architkwatra/CSC-501-Operating_Systems/tree/master/PA1", 
+      PA2: "https://github.com/architkwatra/CSC-501-Operating_Systems/tree/archit/PA2", 
+      PA3: "https://github.com/architkwatra/CSC-501-Operating_Systems/tree/archit/csc501-lab3",
+    },
+    textSummarization: "https://github.com/architkwatra/NewsArticleSummarixation",
+    scrumBot: "https://github.ncsu.edu/akwatra/ScrumBot",
+    dxl: "https://www.dxl.com/mens-big-and-tall-store",
+    dsw: "https://www.dsw.com/en/us/",
+    networking: {
+      goBackN: "https://github.com/architkwatra/FTP/tree/main/Go_Back_N", 
+      selectiveRepeat: "https://github.com/architkwatra/FTP/tree/main/SelectiveARQ",
+      p2p: "https://github.com/architkwatra/peer-to-peer_with_CI"
+    },
+    sentimentAnalysis: "https://github.com/architkwatra/Sentiment-Analysis"
+  };
+
   ngOnInit(): void {
+    
   }
 
 }
